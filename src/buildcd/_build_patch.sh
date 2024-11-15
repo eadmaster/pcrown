6ffba7181f25a55eedf96bb15371e36821e09260
eadmaster <925171+eadmaster@users.noreply.github.com>
2024-11-15 04:50:55 +0100

    fix items strings overlapping (#57)

--- src/buildcd/_build_patch.sh
+++ src/buildcd/_build_patch.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Main patching script
+# Made by eadmaster for the Princess Crown Translation GPL Edition project  https://github.com/eadmaster/pcrown/
+
+# Requirements:
+# - apt install iat  p7zip-full  xdelta3  wine   # Ubuntu 22.04 LTS packages
+# - cd-replace (manual install) https://github.com/mistydemeo/cd-replace
+# - ucon64 (manual install)  https://ucon64.sourceforge.io/
+# - skf (manual install)  https://sourceforge.net/projects/swissfileknife/
+# - eventeditor.exe -> build via ../eventeditor/_build.sh
+# - itemsutil.exe -> build via ../itemsutil/_build.sh
 
 # cleanup
 rm *.eng
@@ -14,7 +24,7 @@ cp "Princess Crown (Japan) (1M) (Track 01) (English).iso" "Princess Crown (Japan
 
 # update font
 7z e -y "Princess Crown (Japan) (1M) (Track 01) (English).iso" KANJI.BIN
-xdelta3 -d -s KANJI.BIN KANJI.BIN.xdelta KANJI_ENG.BIN  # apply English font patch
+xdelta3 -f -d -s KANJI.BIN KANJI.BIN.xdelta KANJI_ENG.BIN  # apply English font patch
 #xdelta3 -e -s KANJI.BIN  KANJI_ENG.BIN  KANJI.BIN.xdelta  # create new font patch
 
 TRANSLATED_SCRIPT_PATH=../../script/eng
@@ -29,9 +39,10 @@ cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" KANJI.BIN  KA
 #060729A8    E204
 #06072994    E204
 #0607297C    E204
-#hexcalc 0x060729A8-0x06004000+1 = 6E9A9
-#hexcalc 0x06072994-0x06004000+1 = 6E995
-#hexcalc 0x0607297C-0x06004000+1 = 6E97D
+#hexcalc '060729A8 - 06004000 + 1' = 6E9A9
+#hexcalc '06072994 - 06004000 + 1' = 6E995
+#hexcalc '0607297C - 06004000 + 1' = 6E97D
+# TODO: replace ucon64 -> sfk setbytes  http://stahlworks.com/sfk-setbytes
 ucon64 --nbak --poke=6E9A9:04 0.BIN
 ucon64 --nbak --poke=6E995:04 0.BIN
 ucon64 --nbak --poke=6E97D:04 0.BIN
@@ -49,6 +60,42 @@ ucon64 --nbak --poke=7001B:04 0.BIN
 ucon64 --nbak --poke=70021:04 0.BIN
 ucon64 --nbak --poke=702EF:88 0.BIN
 ucon64 --nbak --poke=702BB:3A 0.BIN
+
+# items description fixes (thanks to paul_met) https://github.com/eadmaster/pcrown/issues/57
+#hexcalc '06074602 - 06004000 + 1' =70603
+ucon64 --nbak --poke=70603:1A 0.BIN  # X position of the price in the store right-aligned
+# Tile map address (window #1)
+ucon64 --nbak --poke=47732:B3 0.BIN
+ucon64 --nbak --poke=47733:08 0.BIN
+# Tile map address (window #2)
+ucon64 --nbak --poke=3BE0A:B3 0.BIN
+ucon64 --nbak --poke=3BE0B:08 0.BIN
+# Height of window #1
+ucon64 --nbak --poke=3BE09:05 0.BIN
+# Height of window #2
+ucon64 --nbak --poke=3BDD5:05 0.BIN
+# Y position of window #1
+ucon64 --nbak --poke=47707:17 0.BIN
+# Y position of window #2
+ucon64 --nbak --poke=3BDDD:17 0.BIN
+# Y position of text
+ucon64 --nbak --poke=75F7E:BC 0.BIN
+# Line Scroll Range Fix
+ucon64 --nbak --poke=3BA61:B8 0.BIN
+# Clearing the description window
+ucon64 --nbak --poke=47773:05 0.BIN
+ucon64 --nbak --poke=4777B:17 0.BIN
+# cook menu fix
+#05E6D56B-    FFEAFFFFEB00FFF3F700FFF7CFFFE9FFE8FF (old value)
+#05E6D57C     FFEAFFFFF7FFE8FFFFE9FFF7FFFFFFFFFFFF (new value)
+#05E6D57D-    FFEAFFEBFF00FFF7003CFFECC6FFFFE8FFF7FFFFE9 (old value)
+#05E6D591     FFEB00FFFFF7FFECC6FFFFEB04FFFFE8FFF7FFFFE9 (new value)
+#05E6D592-    FFEAFFFFEB00FFF7CF00FFECC63FFFE9FFE8FF  (old value)
+#05E6D5A4     FFEAFFFFF7FFECC6FFFFEB05FFFFE8FFF7FFFFF9 (new value)
+sfk replace KANJI_ENG.BIN -binary /FFEAFFFFEB00FFF3F700FFF7CFFFE9FFE8FF/FFEAFFFFF7FFE8FFFFE9FFF7FFFFFFFFFFFF/  -yes
+sfk replace KANJI_ENG.BIN -binary /FFEAFFEBFF00FFF7003CFFECC6FFFFE8FFF7FFFFE9/FFEB00FFFFF7FFECC6FFFFEB04FFFFE8FFF7FFFFE9/  -yes
+#NO? optional (for selling items - adding descriptions line for items). -> breaks the game
+#sfk replace KANJI_ENG.BIN -binary /FFEAFFFFEB00FFF7CF00FFECC63FFFE9FFE8FF/FFEAFFFFF7FFECC6FFFFEB05FFFFE8FFF7FFFFF9/  -yes
 
 cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 0.BIN  0.BIN
 
@@ -77,4 +124,4 @@ done
 cd-replace  "Princess Crown (Japan) (1M) (Track 01) (English).iso" 015_00_1.EVN 015_00_1.EVN
 
 # build xdelta patch
-xdelta3 -e -s "Princess Crown (Japan) (1M) (Track 01) (English).iso.bak"  "Princess Crown (Japan) (1M) (Track 01) (English).iso"  "Princess.Crown.Japan.1M.Track.01.iso.xdelta"
+xdelta3 -f -e -s "Princess Crown (Japan) (1M) (Track 01) (English).iso.bak"  "Princess Crown (Japan) (1M) (Track 01) (English).iso"  "Princess.Crown.Japan.1M.Track.01.iso.xdelta"
--- src/buildcd/_install_prereq.sh
+++
@@ -1,7 +0,0 @@
- 
-
-apt install iat   p7zip-full  xdelta3  
-
-# build on your own https://github.com/mistydemeo/cd-replace
-# https://ucon64.sourceforge.io/
-
