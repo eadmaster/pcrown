8510b40613408b42256e13d60f387f602cfa66e8
eadmaster <925171+eadmaster@users.noreply.github.com>
2025-02-28 23:27:05 +0100

    repatch COCKPIT.CHB and KD2T.CHR for the EX build instead of using xdelta patches (#18)

--- src/buildcd_ex/_build.sh
+++ src/buildcd_ex/_build.sh
@@ -1,11 +1,13 @@
  #!/bin/bash
  
- 
+# Main patching script for the EX build  https://github.com/eadmaster/pcrown/issues/18
+
 # Requirements:
 # - pre-patched "../buildcd/Princess Crown (Japan) (1M) (Track 01) (patched).bin"
 # - Princess Crown-Hack_(v1.1).7z  downloaded from https://meduza-team.ucoz.net/publ/romkhaking/modifikacii/modifikacija_igr_dlja_sega_saturn/3-1-0-5
 
 export PATCHED_IMAGE_FILE="Princess Crown (Japan) (1M) (Track 01) (patched).bin"
+export SIGNS_PATH=../../signs
 
 rm "$PATCHED_IMAGE_FILE"
 cp "../buildcd/$PATCHED_IMAGE_FILE" .
@@ -22,15 +24,15 @@ done
 cd-replace  "$PATCHED_IMAGE_FILE"  STG_DAT2.PRG  STG_DAT2.PRG  > /dev/null
 cd-replace  "$PATCHED_IMAGE_FILE"  COCKPIT.CLB  COCKPIT.CLB  > /dev/null
 
-# replace BEGIN text in load save dialog  https://github.com/eadmaster/pcrown/issues/90
-# TODO: repatch
-#file_patch COCKPIT.CHB $(file2hexstr ${SIGNS_PATH}/save_begin_jap.bin) $(file2hexstr ${SIGNS_PATH}/save_begin_eng.bin)
-xdelta3 -f -d -s COCKPIT.CHB COCKPIT.CHB.xdelta COCKPIT.CHB.eng
-cd-replace  "$PATCHED_IMAGE_FILE" COCKPIT.CHB COCKPIT.CHB.eng > /dev/null
+# repatch COCKPIT.CHB - replace BEGIN text in load save dialog  https://github.com/eadmaster/pcrown/issues/90
+file2hexstr() { sfk hexdump -raw "$1" ; }
+sfk replace COCKPIT.CHB -binary /$(file2hexstr ${SIGNS_PATH}/save_begin_jap.bin)/$(file2hexstr ${SIGNS_PATH}/save_begin_eng.bin)/ -yes -firsthit
+cd-replace  "$PATCHED_IMAGE_FILE" COCKPIT.CHB COCKPIT.CHB > /dev/null
 
-# TODO: repatch the signs
-# source _patch_signs.sh
-xdelta3 -f -d -s KD2T.CHR KD2T.CHR.xdelta KD2T.CHR.eng
+# repatch KD2T.CHR
+cp "../buildcd/KD2T.CHR" KD2T.CHR.eng
+sfk replace KD2T.CHR.eng -binary /11111111111111111111111111111111111111111111111111111111111111112222222222222222222222222222222222222222222222222222222222222222/44444444444444444444444444444444444444444444444444444444444444445555555555555555555555555555555555555555555555555555555555555555/ -yes -firsthit
+sfk replace KD2T.CHR.eng -binary /3333333333333333333333333333333333333333333333333333333333333333/6666666666666666666666666666666666666666666666666666666666666666/ -yes -firsthit                                  
 cd-replace  "$PATCHED_IMAGE_FILE" KD2T.CHR KD2T.CHR.eng  > /dev/null
 
 # repatch 0.BIN, KANJI.BIN
